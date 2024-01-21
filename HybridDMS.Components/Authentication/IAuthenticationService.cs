e4643a606f916a61e468dc9444006a8b0fd9db34
Johannes Soedjono <antonius.ivan.han@gmail.com>
2024-01-21 16:06:45 +0700

    FIrst BUild Single RCL Class Done

---
+++ HybridDMS.Components/Authentication/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using HybridCisBCRM.Components.Models;
+
+namespace HybridCisBCRM.Components.Authentication
+{
+    public interface IAuthenticationService
+    {
+        Task<AuthenticatedUserModel> Login(AuthenticationUserModel userForAuthentication);
+        Task LogOut();
+    }
+}
---
+++ HybridDMS.Components/Data/TodoService.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+
+namespace HybridCisBCRM.Components.Data
+{
+    public class TodoService
+    {
+        string file = string.Empty;
+
+        public TodoService() 
+        {
+            file = Path.Combine(Environment.GetFolderPath(
+                Environment.SpecialFolder.ApplicationData
+                ), "item.json");
+        }
+
+        public void SaveItems(IEnumerable<TodoItem> items)
+        {
+            File.WriteAllText(file, JsonSerializer.Serialize(items));
+        }
+
+        public IEnumerable<TodoItem> LoadItems() 
+        { 
+            if(!File.Exists(file)) 
+            { 
+                return Enumerable.Empty<TodoItem>();
+            }
+
+            var itemJson = File.ReadAllText(file);
+            return JsonSerializer.Deserialize<IEnumerable<TodoItem>>(itemJson) ?? 
+                Enumerable.Empty<TodoItem>();
+        }
+    }
+}
