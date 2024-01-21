8b9ad4c18188920d04362e1f45ab586f7a079bf2
Johannes Soedjono <antonius.ivan.han@gmail.com>
2024-01-21 16:50:34 +0700

    so far so good integrate

---
+++ HybridDMS.Components/Item/ItemHelper.cs
@@ -0,0 +1,9 @@
+﻿using HybridDMS.Components.Catalog;
+
+namespace HybridDMS.Components.Item;
+
+public static class ItemHelper
+{
+    public static string Url(CatalogItem item)
+        => $"item/{item.Id}";
+}
---
+++ HybridDMS.Components/Services/IProductImageUrlProvider.cs
@@ -0,0 +1,11 @@
+﻿using HybridDMS.Components.Catalog;
+
+namespace HybridDMS.Components.Services;
+
+public interface IProductImageUrlProvider
+{
+    string GetProductImageUrl(CatalogItem item)
+        => GetProductImageUrl(item.Id);
+
+    string GetProductImageUrl(int productId);
+}
